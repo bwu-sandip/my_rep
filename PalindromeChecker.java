a6ee56f6441ef0d6c541730956bf073ead323de3
SANDIP KUNDU BWU <bwubta22449@brainwareuniversity.ac.in>
2024-11-20 16:02:17 +0530

    PalindromeChecker.java

---
+++ PalindromeChecker.java
@@ -0,0 +1,27 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String input = "racecar";  
+        boolean isPalindrome = isPalindrome(input);
+        
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
